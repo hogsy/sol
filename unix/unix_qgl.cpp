37afd5ec989b0641aebaf13a1c299623509c05bf
Mark Sowden <hogsy@oldtimes-software.com>
2024-12-10 21:42:36 +0000

    Reset this back, wasn't supposed to be included.

--- unix/unix_qgl.cpp
+++ unix/unix_qgl.cpp
@@ -3150,7 +3150,7 @@ qboolean QGL_Init (const char *dllname)
 	SDL_FreeSurface(icon);
 #endif
 	glw_state.glContext = SDL_GL_CreateContext(glw_state.glWindow);
-	SDL_SetRelativeMouseMode (SDL_FALSE);
+	SDL_SetRelativeMouseMode (SDL_TRUE);
 
 	glConfig.allowCDS = true;
 	glConfig.have_stencil = true;
